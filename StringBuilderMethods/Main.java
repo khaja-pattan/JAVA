a9611f7d752160e5a1b1a338f09251cd50d2736d
khaja-pattan <pkhajap768@gmail.com>
2026-02-10 21:33:06 +0530

    update

--- StringBuilderMethods/Main.java
+++ StringBuilderMethods/Main.java
@@ -6,42 +6,35 @@ public class Main {
         StringBuilder bf2 = new StringBuilder("hi   kello");
         bf.append("hi   hello");
 
+        String s = bf.toString();
+        bf.trimToSize();
 
-        
-      
-        //    String s = bf.toString();
-      // bf.trimToSize(); 
+        bf.isEmpty(); // return ture if string is empty
 
-     // bf.isEmpty();
-    
-     System.out.println( bf.length());  // return the string length
+        System.out.println(bf.length()); // return the string length
 
-    
-   //System.out.println(bf.substring(4)); ;
-   
-   System.out.println(bf.substring(1,6));  // return the substring at that index from 1 to 6
+        System.out.println(bf.substring(4)); // return substring form 4 to end of the stirng
 
-    bf.codePointAt(1); // return ascii value 
+        System.out.println(bf.substring(1, 6)); // return the substring at that index from 1 to 6
 
-   bf.insert(0,'h');   //  at  0 index add "h"  "hi hello" becomes "hhi hello"
-   
-   System.out.println(bf);
-    bf.setCharAt(0,'n');     //  replace character at 0 index with "n"
+        bf.codePointAt(1); // return ascii value
 
-        System.out.println(bf.charAt(1));  /// return character at particuler index 
-       
-        System.out.println(bf.compareTo(bf2));  //  compare the string the method in lexicographicla order 
-        
-        bf.chars().forEach(System.out::println);  /// resunt the assci values 
+        bf.insert(0, 'h'); // at 0 index add "h" "hi hello" becomes "hhi hello"
 
-        System.out.println("capacity : "+bf.capacity());    // return  capatiry 
-       //System.out.println( bf.charAt(0));
-       // bf.deleteCharAt(0);  // delate charAt index 
+        System.out.println(bf);
+        bf.setCharAt(0, 'n'); // replace character at 0 index with "n"
 
-        // bf.reverse();
+        System.out.println(bf.charAt(1)); /// return character at particuler index
 
+        System.out.println(bf.compareTo(bf2)); // compare the string the method in lexicographicla order
 
+        bf.chars().forEach(System.out::println); /// resunt the assci values
 
-      
+        System.out.println("capacity : " + bf.capacity()); // return capatiry
+
+        bf.deleteCharAt(0); // delate charAt index
+
+        bf.reverse(); // it reverse the whole string
+
     }
 }
